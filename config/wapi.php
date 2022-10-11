fcfce15930f4836ba2569c8c6805a9cc9fe81bec
jakuborava <Jakuborava@gmail.com>
2022-10-11 13:55:44 +0200

    config rename

--- config/wapi.php
+++ config/wapi.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'user' => env('WEDOS_CLIENT_USER', 'test'),
-    'password' => env('WEDOS_CLIENT_PASSWORD', '123456'),
-    'api_url' => env('WEDOS_CLIENT_API_URL', 'https://api.wedos.com/wapi/json'),
+    'user' => env('WAPI_USER', 'test'),
+    'password' => env('WAPI_PASSWORD', '123456'),
+    'url' => env('WAPI_URL', 'https://api.wedos.com/wapi/json'),
 ];
+
